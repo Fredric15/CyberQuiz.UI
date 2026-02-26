1df8a56b5b4d046aaa787c98aef9c353c4819860
Viggo Krona <viggo.krona@gmail.com>
2026-02-26 14:50:17 +0100

    Added services and Interfaces

---
+++ CyberQuiz.BLL/Services/CategoryService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuiz.BLL.Services
+{
+    //public class CategoryService : ICategoryService
+    //{
+    //    // !!ändra till rätt databas context senare 
+    //    //private readonly CyberQuizDbContext _context;
+
+    //    public CategoryService(CyberQuizDbContext context)
+    //    {
+    //        _context = context;
+    //    }
+
+    //    public async Task<IEnumerable<Category>> GetCategories()
+    //    {
+    //        return await _context.Categories.ToListAsync();
+    //    }
+
+    //    public async Task<IEnumerable<SubCategory>> GetSubCategoriesByCategoryId(int categoryId)
+    //    {
+    //        return await _context.SubCategories
+    //            .Where(s => s.CategoryId == categoryId)
+    //            .ToListAsync();
+    //    }
+    //}
+
+}
