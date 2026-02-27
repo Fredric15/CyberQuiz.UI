4781300f40e8545d27983f558784d29963bf0089
Fredric Törnkvist <fredricff@hotmail.com>
2026-02-27 09:44:05 +0100

    Lagt till UserScore-repo

---
+++ CyberQuiz.DAL/Repositories/Interfaces/IUserScoreRepository.cs
@@ -0,0 +1,16 @@
+﻿using CyberQuiz.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuiz.DAL.Repositories.Interfaces
+{
+    public interface IUserScoreRepository
+    {
+        // Hämtar alla poäng för en specifik användare
+        Task<IEnumerable<UserScoreModel>> GetUserScoreByUserIdAsync(string userId);
+        
+        // Lägger till en ny poäng för en användare
+        Task AddScoreAsync(UserScoreModel userScore);
+    }
+}
---
+++ CyberQuiz.DAL/Repositories/UserScoreRepository.cs
@@ -0,0 +1,33 @@
+﻿using CyberQuiz.DAL.Data;
+using CyberQuiz.DAL.Models;
+using CyberQuiz.DAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CyberQuiz.DAL.Repositories
+{
+    public class UserScoreRepository : IUserScoreRepository
+    {
+        private readonly AppDbContext _context;
+        public UserScoreRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddScoreAsync(UserScoreModel userScore)
+        {
+            //Skapar ett nytt UserScore-objekt och lägger till den i databasen
+            await _context.UserScores.AddAsync(userScore);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<UserScoreModel>> GetUserScoreByUserIdAsync(string userId)
+        {
+            // Hämtar alla UserScore-objekt som matchar det angivna userId och returnerar dem som en lista
+            return await _context.UserScores
+                .Where(us => us.ApplicationUserId == userId)
+                .ToListAsync();
+        }
+    }
+}
