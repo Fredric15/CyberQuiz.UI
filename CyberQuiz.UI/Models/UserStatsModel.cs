aa922d50cdb03df656e21cbf92f6f34549dc18e4
Jonathan Junge <jonathan.junge96@gmail.com>
2026-03-02 17:14:45 +0100

    Fixed NavBar, added pages, fixed routing

---
+++ CyberQuiz.UI/Models/UserStatsModel.cs
@@ -0,0 +1,20 @@
+﻿namespace CyberQuiz.UI.Models;
+
+public class UserStatsModel
+{
+    public int CorrectAnswers { get; set; }
+    public int TotalQuestions { get; set; }
+
+    // Basic logik för denna lilla uträkningen
+    public int Percentage
+    {
+        get
+        {
+            if (TotalQuestions == 0)
+                return 0;
+
+            return (int)((double)CorrectAnswers / TotalQuestions * 100);
+        }
+    }
+
+}
