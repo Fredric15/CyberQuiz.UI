11864a1ff5861656c4a8f40cfd0078503eee39ac
Jonathan Junge <jonathan.junge96@gmail.com>
2026-03-03 18:57:04 +0100

    Updated to match BLL

---
+++ CyberQuiz.UI/Models/ChangePasswordModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CyberQuiz.UI.Models;
+
+public class ChangePasswordModel
+{
+    [Required(ErrorMessage = "Current password is required.")]
+    public string CurrentPassword { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "New password is required.")]
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "New password must be at least 8 characters.")]
+    public string NewPassword { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Please confirm your new password.")]
+    [Compare(nameof(NewPassword), ErrorMessage = "Passwords do not match.")]
+    public string ConfirmNewPassword { get; set; } = string.Empty;
+}
