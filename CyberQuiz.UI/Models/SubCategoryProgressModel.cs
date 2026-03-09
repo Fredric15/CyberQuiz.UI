8c15b8e1870a6be4286eb62e8f546ba7c6e409bc
Viggo Krona <viggo.krona@gmail.com>
2026-03-09 10:19:40 +0100

    fixed so the next quiz gets unlocked.

--- CyberQuiz.UI/Models/SubCategoryProgressModel.cs
+++ CyberQuiz.UI/Models/SubCategoryProgressModel.cs
@@ -1,9 +1,17 @@
+using System.Text.Json.Serialization;
+
 namespace CyberQuiz.UI.Models;
 
 public class SubCategoryProgressModel
 {
+    [JsonPropertyName("subCategoryModelId")]
     public int SubCategoryId { get; set; }
+
     public double ScorePercentage { get; set; }
+
+    [JsonPropertyName("isCompleted")]
     public bool IsPassed { get; set; }
+
+    [JsonPropertyName("unlockedAt")]
     public DateTime CompletedAt { get; set; }
 }
